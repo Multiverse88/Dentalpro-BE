a05a7e2e2e9e2a4ba66e0de7d22007d891216916
Multiverse88 <ainanihj@gmail.com>
2025-07-21 18:59:26 +0700

    update vercel

--- src/app/api/appointments/[id]/route.ts
+++ src/app/api/appointments/[id]/route.ts
@@ -1,15 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../../../db/prisma';
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  const id = Number(params.id);
+export async function PUT(req: NextRequest, context: any) {
+  const id = Number(context.params.id);
   const data = await req.json();
   const appt = await prisma.appointment.update({ where: { id }, data });
   return NextResponse.json(appt);
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-  const id = Number(params.id);
+export async function DELETE(req: NextRequest, context: any) {
+  const id = Number(context.params.id);
   const appt = await prisma.appointment.delete({ where: { id } });
   return NextResponse.json(appt);
 } 
